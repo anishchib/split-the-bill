5e2de28fbb517dae66914443b837c62c1d857cac
anish chib <anish.chib@gmail.com>
2025-11-14 14:14:02 +0530

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,6 @@ function App() {
       <div>
         <h1>Vite + React</h1>
       </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
