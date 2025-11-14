738c910979d5945afcf27d630696fd824d95ff63
anish chib <anish.chib@gmail.com>
2025-11-14 15:50:43 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Header } from "./Header.jsx";
+import { Footer } from "./Footer.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,19 +14,12 @@ function App() {
       <div className="body-div">
         <div className="body-sub-div">
           <h2>Welcome to Split-The-Bill App</h2>
-          <p>
-            This is a simple React application to help you split bills among
-            friends easily and fairly.
-          </p>
         </div>
         <div className="body-sub-div">
           <h3>Get Started</h3>
-          <p>
-            To get started, add the expenses and the app will calculate how much
-            each person owes.
-          </p>
         </div>
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="footer-div">
+      <p>
+        © 2025 Split-The-Bill App. All rights reserved.
+        <span>by Anish Chib</span>
+      </p>
+    </div>
+  );
+};
