36890232ce9b6bcb0bd51f28c4d72ff96f82b00f
anish chib <anish.chib@gmail.com>
2025-11-14 14:53:31 +0530

    commit at 3 pm 14-11-2025

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Header } from "./Header.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <h1>Vite + React</h1>
+      <Header />
+      <div className="body-div">
+        <div className="body-sub-div">
+          <h2>Welcome to Split-The-Bill App</h2>
+          <p>
+            This is a simple React application to help you split bills among
+            friends easily and fairly.
+          </p>
+        </div>
+        <div className="body-sub-div">
+          <h3>Get Started</h3>
+          <p>
+            To get started, add the expenses and the app will calculate how much
+            each person owes.
+          </p>
+        </div>
       </div>
     </>
   );
