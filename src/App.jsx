7c062dea2ccd2bfe7100d71e8a669e350b364e7d
anish chib <anish.chib@gmail.com>
2025-11-14 16:10:34 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Header } from "./Header.jsx";
 import { Footer } from "./Footer.jsx";
+import { Button } from "./Button.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,10 +14,12 @@ function App() {
       <Header />
       <div className="body-div">
         <div className="body-sub-div">
-          <h2>Welcome to Split-The-Bill App</h2>
+          <h2>Welcome to Split-The-Bill App by Anish</h2>
+          <Button>Add User</Button>
         </div>
         <div className="body-sub-div">
-          <h3>Get Started</h3>
+          <h2>Get Started Here fpr App Working</h2>
+          <Button> Submit </Button>
         </div>
       </div>
       <Footer />
